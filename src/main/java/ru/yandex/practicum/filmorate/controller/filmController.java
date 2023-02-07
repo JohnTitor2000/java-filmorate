85d3f4576885b6fdd98da86e0be57ee49ecd4790
Урику Виталий <johntitor2000@yandex.ru>
2023-02-07 17:47:23 +0300

    refactor: Небольшие правки контроллеров.

--- src/main/java/ru/yandex/practicum/filmorate/controller/filmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/filmController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.ValidationException;
@@ -15,35 +16,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class filmController {
     private static final LocalDate FIRST_FILM_RELEASE = LocalDate.of(1895, 11, 28);
     private Map<Integer, Film> data = new HashMap<>();
     private static int currentId = 0;
 
-    @GetMapping("/films")
+    @GetMapping
     public Collection<Film> findAll() {
         return data.values();
     }
 
-    @PostMapping("/films")
+    @PostMapping
     public Film create(@Valid @RequestBody Film film) {
-        if (film.getReleaseDate().isBefore(FIRST_FILM_RELEASE)) {
-            throw new ValidationException("The release of the film cannot be earlier than December 11, 1895.");
-        }
+        filmDataValidate(film);
         film.setId(++currentId);
         data.put(film.getId(), film);
         return film;
     }
 
-    @PutMapping("/films")
+    @PutMapping
     public Film filmUpdate(@Valid @RequestBody Film film) {
         if(!data.containsKey(film.getId())) {
             throw new ValidationException("This movie does not exist.");
         }
+        filmDataValidate(film);
+        data.put(film.getId(), film);
+        return data.get(film.getId());
+    }
+
+    private void filmDataValidate(Film film) {
         if (film.getReleaseDate().isBefore(FIRST_FILM_RELEASE)) {
             throw new ValidationException("The release of the film cannot be earlier than December 11, 1895.");
         }
-        data.put(film.getId(), film);
-        return data.get(film.getId());
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/userController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/userController.java
@@ -1,49 +1,53 @@
-package ru.yandex.practicum.filmorate.controller;
+    package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.model.ValidationException;
+    import lombok.extern.slf4j.Slf4j;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.PutMapping;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
+    import ru.yandex.practicum.filmorate.model.User;
+    import ru.yandex.practicum.filmorate.model.ValidationException;
 
-import javax.validation.Valid;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+    import javax.validation.Valid;
+    import java.util.Collection;
+    import java.util.HashMap;
+    import java.util.Map;
 
-@Slf4j
-@RestController
-public class userController {
-    private Map<Integer, User> data = new HashMap<>();
-    private static int currentId = 0;
+    @Slf4j
+    @RestController
+    @RequestMapping("/films")
+    public class userController {
+        private Map<Integer, User> data = new HashMap<>();
+        private static int currentId = 0;
 
-    @GetMapping("/users")
-    public Collection<User> findAll() {
-        return data.values();
-    }
-
-    @PostMapping("/users")
-    public User create(@Valid @RequestBody User user) {
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
+        @GetMapping
+        public Collection<User> findAll() {
+            return data.values();
         }
-        user.setId(++currentId);
-        data.put(user.getId(), user);
-        return user;
-    }
 
-    @PutMapping("/users")
-    public User userUpdate(@Valid @RequestBody User user) {
-        if(!data.containsKey(user.getId())) {
-            throw new ValidationException("This user does not exist.");
+        @PostMapping
+        public User create(@Valid @RequestBody User user) {
+            nameValidation(user);
+            user.setId(++currentId);
+            data.put(user.getId(), user);
+            return user;
         }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
+
+        @PutMapping
+        public User userUpdate(@Valid @RequestBody User user) {
+            if(!data.containsKey(user.getId())) {
+                throw new ValidationException("This user does not exist.");
+            }
+            nameValidation(user);
+            data.put(user.getId(), user);
+            return data.get(user.getId());
         }
-        data.put(user.getId(), user);
-        return data.get(user.getId());
+
+        private void nameValidation(User user) {
+            if (user.getName() == null || user.getName().isBlank()) {
+                user.setName(user.getLogin());
+            }
+        }
     }
-}
